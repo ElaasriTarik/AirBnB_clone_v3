2fa8bf12254e58f1df712d95072706da24ad574f
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-23 22:05:09 +0100

    v

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,6 +21,6 @@ def call_close(err):
 
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    port = int(os.environ.get('HBNB_API_PORT', '5000'))
 
     app.run(host=host, port=port, threaded=True)
