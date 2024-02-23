413e08034c4cebd9d01f7ba52479a08c1bcaecc4
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-23 22:10:59 +0100

    v

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,11 +4,12 @@ index
 """
 
 
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def ok():
-    """status ok"""
-    return (jsonify(status="ok"))
+        """status ok"""
+        return (jsonify(status="ok"))
