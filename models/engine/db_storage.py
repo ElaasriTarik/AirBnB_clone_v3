6c5069c4b529d91e6d077769fd0f6285cfefc0c4
Abdessadek EL aasri <elaasriya2003@gmail.com>
2024-02-23 13:36:46 +0000

    db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,8 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """return a class based on its id"""
+        val = [c if cls['id'] is id for x in cls]
+        return val[0]
