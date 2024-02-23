db7d54c9a1d0aa34f1c8b6eb1a58e7639e68e3a4
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-24 00:08:17 +0100

    v

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,7 +9,20 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def ok():
-        """status ok"""
-        return (jsonify(status="ok"))
+    """status ok"""
+    return (jsonify(status="ok"))
+
+@app_views.route('/stats', strict_slashes=False)
+def number_objs():
+    """returns objects numbers"""
+    return (jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        states= storage.count('States'),
+        reviews= storage.count('Review'),
+        places= storage.count('Place'),
+        users= storage.count('User')
+
+        ))
