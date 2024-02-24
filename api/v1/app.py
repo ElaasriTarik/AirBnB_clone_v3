d046de1ad2ebc1b2ddbe93993c00dd45b3884c90
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-24 14:49:41 +0100

    f

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ flask app
 """
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -17,6 +17,11 @@ app.register_blueprint(app_views)
 def call_close(err):
         """storage close()"""
         storage.close()
+
+
+@app.errorhandler(404)
+def not_found(err):
+        return (jsonify(error="Not found"), 404)
 
 
 if __name__ == "__main__":
