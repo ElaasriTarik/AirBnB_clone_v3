120fd10985c56599da5f146d77ba85655e3c45ac
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-25 14:08:37 +0100

    I hated betty and now pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ flask app
 """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -21,7 +21,7 @@ def call_close(err):
 
 @app.errorhandler(404)
 def not_found(err):
-    return (jsonify(error="Not found"), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
