832d48e9ee0e2bcc967349e40e1ee1b02e8d1a24
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-02-26 19:48:18 +0100

    amenity

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
